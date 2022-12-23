c0741271345621a09be988fd4ea58ab60733386f
Krysha <akai_sakura@outlook.com>
2022-12-23 20:30:53 +0800

    feat: 对接新推送接口

--- assets/Scripts/scenes/Main/MySeed.ts
+++ assets/Scripts/scenes/Main/MySeed.ts
@@ -1,39 +1,41 @@
-import { _decorator, Component, Node } from 'cc';
-import {AtalsLabel} from "db://assets/Scripts/ext/AtalsLabel";
-import {RequestController, RequestMapping} from "db://assets/Scripts/utils/net";
+import { _decorator, Component, Node } from "cc";
+import { AtalsLabel } from "db://assets/Scripts/ext/AtalsLabel";
+import {
+  RequestController,
+  RequestMapping,
+} from "db://assets/Scripts/utils/net";
 const { ccclass, property } = _decorator;
 
-@ccclass('SeedItem')
+@ccclass("SeedItem")
 class SeedItem {
-    @property
-    id: number = 0;
+  @property
+  id: number = 0;
 
-    @property(AtalsLabel)
-    label: AtalsLabel = null;
+  @property(AtalsLabel)
+  label: AtalsLabel = null;
 }
 
-@ccclass('MySeed')
+@ccclass("MySeed")
 @RequestController
 export class MySeed extends Component {
+  @property(SeedItem)
+  item: SeedItem[] = [];
 
-    @property(SeedItem)
-    item: SeedItem[] = [];
+  start() {}
 
-    start() {
+  update(deltaTime: number) {}
 
+  @RequestMapping("MY_DATA")
+  updateSeedNum({ assets }: { assets: Record<string, string> }) {
+    for (let item of this.item) {
+      item.label.str = assets[item.id];
     }
+  }
 
-    update(deltaTime: number) {
-        
+  @RequestMapping("ASSETS")
+  updateSeedNum1({ assets }: { assets: Record<string, string> }) {
+    for (let item of this.item) {
+      item.label.str = assets[item.id];
     }
-
-    @RequestMapping('MY_DATA')
-    updateSeedNum({assets}: {assets: Record<string, string>;}) {
-        for (let item of this.item) {
-            item.label.str = assets[item.id];
-        }
-    }
-
+  }
 }
-
-
